76a82bcedb3efd4216590510feea709c9b3fdf59
Fworg64 <fworg64@gmail.com>
2019-04-18 02:09:15 -0600

    looks like we cant integrate

--- wheelaccelDynamics.m
+++ wheelaccelDynamics.m
@@ -10,7 +10,7 @@
 %d3y = 
 
 syms theta omega Ul Ur dx dy d2x d2y d3x d3y dUl dUr d2Ul d2Ur
-dt = .002; AxelLen = .62;
+dt = .02; AxelLen = .62;
 
 dx = (AxelLen/2)*(Ur + Ul)/(Ur - Ul) * (cos(theta)*sin((Ur - Ul) / AxelLen)... %take dt off and then do expm?
                                  + sin(theta)*cos((Ur - Ul) / AxelLen) - sin(theta));
@@ -22,32 +22,53 @@ dy = (AxelLen/2)*(Ur + Ul)/(Ur - Ul) * (sin(theta)*sin((Ur - Ul) / AxelLen)...
 %alpha;% = (dUr - dUl)/AxelLen;
 %beta;% = (d2Ur - d2Ul)/AxelLen;
 
-d2x = diff(dx, Ul)*dUl + diff(dx, Ur)*dUr...
-    + diff(dx, theta)*(Ur - Ul) / AxelLen;
-d2y = diff(dy, Ul)*dUl + diff(dy, Ur)*dUr...
-    + diff(dy, theta)*(Ur - Ul) / AxelLen;
+%d2x = diff(dx, Ul)*dUl + diff(dx, Ur)*dUr...
+%    + diff(dx, theta)*(Ur - Ul) / AxelLen;
+%d2y = diff(dy, Ul)*dUl + diff(dy, Ur)*dUr...
+%    + diff(dy, theta)*(Ur - Ul) / AxelLen;
+firstordermodel_dx = matlabFunction(dx);
+firstordermodel_dy = matlabFunction(dy);
 
-d3x = diff(d2x, Ul)*dUl      + diff(d2x, Ur)*dUr...
-    + diff(d2x, dUl)*d2Ul    + diff(d2x, dUr)*d2Ur...
-    + diff(d2x, theta)*(Ur - Ul) / AxelLen;
-    %+ diff(d2x, omega)*(dUr - dUl)/AxelLen;...
-    %+ diff(d2x, alpha)*(d2Ul - d2Ur)/AxelLen;
+d2x = (firstordermodel_dx(Ul +dUl*dt, Ur +dUr*dt, theta + dt*(Ur-Ul)/AxelLen)...
+    - firstordermodel_dx(Ul - dUl*dt, Ur -dUr*dt, theta -dt*(Ur-Ul)/AxelLen))/(2*dt);
+d2y = (firstordermodel_dy(Ul +dUl*dt, Ur +dUr*dt, theta + dt*(Ur-Ul)/AxelLen)...
+    - firstordermodel_dy(Ul - dUl*dt, Ur -dUr*dt, theta -dt*(Ur-Ul)/AxelLen))/(2*dt);
 
-d3y = diff(d2y, Ul)*dUl      + diff(d2y, Ur)*dUr...
-    + diff(d2y, dUl)*d2Ul    + diff(d2y, dUr)*d2Ur...
-    + diff(d2y, theta)*(Ur - Ul) / AxelLen;
-    %+ diff(d2y, omega)*alpha...
-    %+ diff(d2y, alpha)*(d2Ul - d2Ur)/AxelLen;
+firstordermodel_d2x = matlabFunction(d2x);
+firstordermodel_d2y = matlabFunction(d2y);
+
+d3x = (firstordermodel_d2x(Ul + dUl*dt, Ur + dUr*dt,...
+                           dUl + d2Ul*dt, dUr + d2Ur*dt,...
+                           theta + dt*(Ur-Ul)/AxelLen)...
+    - firstordermodel_d2x(Ul, Ur, dUl, dUr, theta))/dt;
+d3y = (firstordermodel_d2y(Ul + dUl*dt, Ur + dUr*dt,...
+                           dUl + d2Ul*dt, dUr + d2Ur*dt,...
+                           theta + dt*(Ur-Ul)/AxelLen)...
+    - firstordermodel_d2y(Ul, Ur, dUl, dUr, theta))/dt;
+% 
+% d3x = diff(d2x, Ul)*dUl      + diff(d2x, Ur)*dUr...
+%     + diff(d2x, dUl)*d2Ul    + diff(d2x, dUr)*d2Ur...
+%     + diff(d2x, theta)*(Ur - Ul) / AxelLen;
+%     %+ diff(d2x, omega)*(dUr - dUl)/AxelLen;...
+%     %+ diff(d2x, alpha)*(d2Ul - d2Ur)/AxelLen;
+% 
+% d3y = diff(d2y, Ul)*dUl      + diff(d2y, Ur)*dUr...
+%     + diff(d2y, dUl)*d2Ul    + diff(d2y, dUr)*d2Ur...
+%     + diff(d2y, theta)*(Ur - Ul) / AxelLen;
+%     %+ diff(d2y, omega)*alpha...
+%     %+ diff(d2y, alpha)*(d2Ul - d2Ur)/AxelLen;
 
 
 
 %now take partial wrt each state (given the current input)
 %state = [x,y,theta, dx, dy, d2x, d2y, Ul, Ur, dUl, dUr]
-phi = [0, 0, 0, 0, 0, 0, 0, cos(theta)/2, cos(theta)/2, 0, 0; %dx = dx
-       0, 0, 0, 0, 0, 0, 0, sin(theta)/2, sin(theta)/2, 0, 0; %dy = dy
+phi = [0, 0, diff(dx,theta), 0, 0, 0, 0, diff(dx, Ul),diff(dx,Ur), 0, 0; %dx = dx
+       0, 0, diff(dy,theta), 0, 0, 0, 0, diff(dy, Ul),diff(dy,Ur), 0, 0; %dy = dy
        0, 0, 0, 0, 0, 0, 0, -1/AxelLen, 1/AxelLen, 0, 0; %dth = om
-       0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0; %ddx = d2x
-       0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0; %ddy = d2y
+       0, 0, diff(d2x,theta), 0, 0, 0, 0, ...
+       diff(d2x,Ul), diff(d2x,Ur), diff(d2x,dUl), diff(d2x,dUr); %ddx = d2x
+       0, 0, diff(d2y,theta), 0, 0, 0, 0, ...
+       diff(d2y,Ul), diff(d2y,Ur), diff(d2y,dUl), diff(d2y,dUr); %ddy = d2y
        0, 0, 0, 0, 0, ...
        0, 0, diff(d3x, Ul), diff(d3x, Ur),...
              diff(d3x, dUl),diff(d3x, dUr); %d2x
@@ -85,7 +106,7 @@ phi = [0, 0, 0, 0, 0, 0, 0, cos(theta)/2, cos(theta)/2, 0, 0; %dx = dx
                    .1, .1].^2);
                
 delta_time = .02;
-total_time = 5.3;%480;
+total_time = 6.3;%480;
 time_to_solve = [0:delta_time:total_time];
                
  x = [0; 0; 0;...   x,  y, theta
@@ -94,9 +115,9 @@ time_to_solve = [0:delta_time:total_time];
       0; 0; ...    Ul,  Ur
       0; 0];...   dUl, dUr
  %left_u = @(t) ((t <= 4.0).*(2*(t <= .5) - 1).*2.8.*sin(1*2*pi*t))'
- left_u = @(t) ((t<=4).*((t<=2).*2.8.*sin(2*pi*(t/2)) -...
-                 (t>2).*2.8.*sin(2*pi*(t/2))))';
- U = [left_u(time_to_solve), 2*left_u(time_to_solve)];
+ left_u = @(t) ((t<=6).*((t<=2).*.8.*sin(2*pi*(t/2)) -...
+                 (t>=4).*.8.*sin(2*pi*(t/2))))';
+ U = [1.8*left_u(time_to_solve), 2*left_u(time_to_solve)];
 
  num_runs = length(time_to_solve);
  x_rec = zeros(length(x),num_runs); 
@@ -124,11 +145,11 @@ time_to_solve = [0:delta_time:total_time];
  legend('theta','Ul', 'Ur', 'dUl', 'dUr');
  function [phi, gamma] = robust_phi_state(x, u, phi_func, gamma_func)
  vel_adj = x(8);  
- if (abs(x(8) - x(9)) < .01) 
-       vel_adj = x(8) + .02;
+ if (abs(x(8) - x(9)) < .00001) 
+       vel_adj = x(8) + .00002;
  end
        phi = phi_func(vel_adj, x(9), u(1), u(2), x(10), x(11),x(3));
-       gamma = gamma_func(vel_adj, x(9), x(3));
+       gamma = gamma_func(vel_adj, x(9), u(1), u(2), x(10), x(11),x(3));
        
 % phi = [0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0; %dx = dx
 %        0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0; %dy = dy
